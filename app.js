f5f10a37189e9e19642ed9f840e619fafe4cd1e4
Chris Keyser <ckeyser@cadent.tv>
2019-04-10 14:26:12 -0400

    sdo;uf;

--- app.js
+++ app.js
@@ -4,18 +4,13 @@ const port = process.env.PORT || 3000
 
 app.use(express.json());
 
-app.use(function (req, res, next) {
-    res.header("Content-Type",'application/json');
-    next();
-});
-
 // testing routes go here
-
 app.get("/", function(req, res) {
     res.send("This is the homepage!")
 });
 
 app.put('/marrywizardapi/api/BookKeeping/MatchedWeeks/Cancel', function(req, res) {
+    res.header("Content-Type",'application/json');
     if(req.body.MarriedProposalId && req.body.CancellationDate) {
         res.status(200);
         res.send(JSON.stringify({Success:true}));   
