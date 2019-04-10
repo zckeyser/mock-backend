f55f0c92856e415744f18bbc24b2389aa99592fe
Chris Keyser <ckeyser@cadent.tv>
2019-04-10 14:24:49 -0400

    add some routes

--- app.js
+++ app.js
@@ -11,7 +11,20 @@ app.use(function (req, res, next) {
 
 // testing routes go here
 
+app.get("/", function(req, res) {
+    res.send("This is the homepage!")
+});
 
+app.put('/marrywizardapi/api/BookKeeping/MatchedWeeks/Cancel', function(req, res) {
+    if(req.body.MarriedProposalId && req.body.CancellationDate) {
+        res.status(200);
+        res.send(JSON.stringify({Success:true}));   
+    }
+    else {
+        res.status(400);
+        res.send(JSON.stringify({Success:false}));
+    }
+});
 
 // end routes
 
